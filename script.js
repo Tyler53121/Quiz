9a23e50644821cfe4f888b7b5bc8c32c737ec94c
Tyler <tyler53121@gmail.com>
2019-12-12 21:45:27 -0500

    started JS

---
+++ script.js
@@ -0,0 +1,65 @@
+const QUESTIONS = [
+    {
+        question: '',
+        answers: [
+
+        ]
+        correctAnswer: '';
+    },
+
+    {
+        question: '',
+        answers: [
+
+        ]
+        correctAnswer: '';
+    },{
+        question: '',
+        answers: [
+
+        ]
+        correctAnswer: '';
+    },{
+        question: '',
+        answers: [
+
+        ]
+        correctAnswer: '';
+    },{
+        question: '',
+        answers: [
+
+        ]
+        correctAnswer: '';
+    },{
+        question: '',
+        answers: [
+
+        ]
+        correctAnswer: '';
+    },
+];
+
+
+//these will track the question number and user score
+let questionNumber = 0
+let score = 0
+
+//this will generate the questions on the page
+function generateQuestion() {
+
+};
+
+//this will be responsible for updating the score
+function updateScore() {
+
+};
+
+//this 
+
+//start the quiz
+function start() {
+
+}
+
+
